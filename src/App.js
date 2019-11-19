b2b470a76b7c619b7c494cfea5e884ab361226df
James Meng <james@aytech.ca>
2019-11-18 20:59:26 -0800

    Initial commit from Create React App

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Zoom } from 'react-reveal';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="Filter">
+          <div className="TitleContainer">
+            <p className="Title">JAMES MENG</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
