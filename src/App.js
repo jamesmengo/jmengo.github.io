213d2fc396593007a90896ce21824997110000ce
James Meng <james@aytech.ca>
2019-12-09 11:35:40 -0800

    change CSS structure

--- src/App.js
+++ src/App.js
@@ -4,46 +4,44 @@ import Typist from 'react-typist';
 import Chevron from './assets/chevronDown.svg';
 import { Link, animateScroll as scroll } from 'react-scroll';
 import { Container, Row, Col } from 'react-grid-system';
-
+import { appStyles } from './Style.js';
 import './App.css';
 
-// COnvert the about me page to 3 columns - more effective use ofthe white space
+const styles = appStyles();
 
-const displayStrings = ['Developer', 'Student Leader', 'Critical Thinker']
+// Convert the about me page to 3 columns - more effective use ofthe white space
 
 class App extends Component {
+  renderLandingContainer = () => {
+    return <div className="LandingContainer">
+      <div>
+        <span className="NameFirst">{'James  '}</span>
+        <span className="NameSecond">{' Meng'}</span>
+      </div>
+      <div>
+        <Typist style={{ color: 'white' }}>
+          <Typist.Delay ms={1000} />
+          <span className={'scrollText'}>Developer.</span>
+          <Typist.Delay ms={500} />
+          <span className={'scrollText'}> Student Leader.</span>
+          <Typist.Delay ms={500} />
+          <span className={'scrollText'}> Tech Enthusiast.</span>
+        </Typist>
+      </div>
+      <Fade timeout={2000}>
+        <Link to="AboutMe" spy={true} smooth={true} duration={1000}>
+          <img className={'Chevron'} style={{ color: 'white', height: 24, marginTop: '2rem' }} src={Chevron} alt='view more' />
+        </Link>
+      </Fade>
+    </div>;
+  }
 
 
   render() {
     return (
       <div className="App">
         <div className="Filter">
-          <div className="LandingContainer">
-            <div>
-              <span className="NameFirst">{'James  '}</span>
-              <span className="NameSecond">{' Meng'}</span>
-            </div>
-            <div>
-              <Typist style={{ color: 'white' }}>
-                <Typist.Delay ms={1000} />
-                <span className={'header1'}>Developer. </span>
-                <Typist.Delay ms={500} />
-                <span className={'header1'}> Student Leader. </span>
-                <Typist.Delay ms={500} />
-                <span className={'header1'}> Tech Enthusiast. </span>
-              </Typist>
-            </div>
-            <Fade timeout={2000}>
-              <Link
-                to="AboutMe"
-                spy={true}
-                smooth={true}
-                duration={1000}
-              >
-                <img className={'Chevron'} style={{ color: 'white', height: 24, marginTop: '2rem' }} src={Chevron} alt='view more' />
-              </Link>
-            </Fade>
-          </div>
+          {this.renderLandingContainer()}
           <div className='AboutMeContainer' id='AboutMe'>
             <Fade left delay={400} timeout={2000}>
               <h1 >About Me</h1>
---
+++ src/Style.js
@@ -0,0 +1,27 @@
+export const appStyles = () => ({
+  app: {
+    overflow: "hidden"
+  },
+  appLogo: {
+    overflow: 'hidden'
+  },
+  appHeader: {
+    backgroundColor: '#282c34',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 'calc(10px + 2vmin)',
+    color: 'white',
+  },
+  appLink: {
+    color: '#09d3ac',
+  },
+  filter: {
+    height: '100%',
+    backgroundColor: 'black',
+    opacity: '0.75',
+    zIndex: '-9999',
+  },
+})
